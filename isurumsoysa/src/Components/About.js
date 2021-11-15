ec4db7e54d9f9e5e479ac43a69a8e4a24c3fc345
Isuru Soysa <isurumsoysa@gmail.com>
2021-11-15 22:57:10 +0530

    Menu Bar Complete Routes Established

---
+++ isurumsoysa/src/Components/About.js
@@ -0,0 +1,13 @@
+import {Container} from "react-bootstrap";
+
+function About() {
+    return (
+        <Container fluid>
+            <p>About</p>
+        </Container>
+
+
+    );
+}
+
+export default About;
---
+++ isurumsoysa/src/Components/Experience.js
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+function Experience() {
+    return (
+        <Container fluid>
+            <p>Experience</p>
+        </Container>
+    );
+}
+
+export default Experience;
